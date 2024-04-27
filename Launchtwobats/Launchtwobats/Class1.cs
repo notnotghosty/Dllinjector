991f083e10e99393f4ff5fe727a3dcbbcc0bac36
notnotghosty <153692932+notnotghosty@users.noreply.github.com>
2024-04-26 17:00:09 -0700

    Project Files for my launcher.
    This launches the .bat file and then 20 seconds later injects the redirect dll to get into a game server.

---
+++ Launchtwobats/Launchtwobats/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        // Specify the path to your batch file
+        string batchFilePath = @"C:\Program Files\Destiny\58afdea4-c702-4b4e-995f-e98a4f7ed7ce\FortniteGame\Binaries\Win64\Launcher.bat";
+        string DlllInjectorPath = @"C:\Program Files\Destiny\58afdea4-c702-4b4e-995f-e98a4f7ed7ce\FortniteGame\Binaries\Win64\Dllinjector.exe";
+
+        // Start the batch file
+        Process.Start(batchFilePath);
+
+        // Wait for 20 seconds (20000 milliseconds)
+        Thread.Sleep(20000); // Wait for 20 seconds
+
+        //Start Dll Injector
+        Process.Start(DlllInjectorPath);
+    }
+}
